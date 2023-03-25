073b984b4bb88ea55dcfab46a426a25b7caee0ca
Daniel Correa <yo@danielgara.com>
2023-03-25 11:40:48 -0500

    imp: with tailwind

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,16 +2,31 @@
 @section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
 @section('content')
-<div class="row">
-  @foreach ($viewData["products"] as $product)
-  <div class="col-md-4 col-lg-3 mb-2">
-    <div class="card">
-      <img src="https://laravel.com/img/logotype.min.svg" class="card-img-top img-card">
-      <div class="card-body text-center">
-        <a href="#" class="btn bg-primary text-white">{{ $product["name"] }}</a>
+<main class="space-y-40 mb-12">
+  <div class="relative" id="home">
+    <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+      <div class="relative pt-10 ml-auto">
+        <div class="text-center mx-auto grid grid-cols-4 gap-4">
+          @foreach ($viewData["products"] as $product)
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+            </a>
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $product->getName() }}</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Best product in the world.</p>
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Check product
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+          </div>
+          @endforeach
+        </div>        
       </div>
     </div>
   </div>
-  @endforeach
-</div>
+</main>
 @endsection
